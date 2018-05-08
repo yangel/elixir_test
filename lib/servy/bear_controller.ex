0550569cebf01628916a8b2f3a11d3a55e29b046
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-08 14:29:07 -0400

    added precompiled templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -4,29 +4,18 @@ defmodule Servy.BearController do
 
   alias Servy.Wildthings
   alias Servy.Bear
-
-  @templates_path Path.expand("../../templates", __DIR__)
-
-  defp render(conv, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | status: 200, resp_body: content}
-  end
+  alias Servy.BearView
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.asc_name/2)
-
-    render conv, "index.html.eex", bears: bears
+    %{conv | status: 200, resp_body: BearView.index(bears)}
   end
 
   def show(conv, %{"id" => id}) do
     if bear = Wildthings.bear_by_id(id) do
-      render conv, "show.html.eex", bear: bear
+      %{conv | status: 200, resp_body: BearView.show(bear)}
     else
       %{conv | status: 404, resp_body: "Bear with id #{id} not found"}
     end
