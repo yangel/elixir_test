94e99f1999814f41fc2df4c2aa1b3633575f438f
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-08 09:28:24 -0400

    added file handler module

---
+++ lib/servy/file_handler.ex
@@ -0,0 +1,16 @@
+require Logger
+
+defmodule Servy.FileHandler do
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File Not Found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    Logger.error "Internal Server Error: #{reason}"
+    %{conv | status: 500, resp_body: "File reading error: #{reason}"}
+  end
+end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -8,6 +8,7 @@ defmodule Servy.Handler do
 
   import Servy.Plugins, only: [emojify: 1, track: 1, rewrite_path: 1, log: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   alias Servy.Conv, as: Conv
 
@@ -63,19 +64,6 @@ defmodule Servy.Handler do
 
   def route(%Conv{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File Not Found!"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    Logger.error "Internal Server Error: #{reason}"
-    %{conv | status: 500, resp_body: "File reading error: #{reason}"}
   end
 
   def format_response(%Conv{} = conv) do
