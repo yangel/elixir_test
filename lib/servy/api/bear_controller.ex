a940696233e6ae68d576fff48569e8e584157fbf
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-09 11:35:00 -0400

    fixed lint issue

--- lib/servy/api/bear_controller.ex
+++ lib/servy/api/bear_controller.ex
@@ -1,8 +1,10 @@
 defmodule Servy.Api.BearController do
   @moduledoc false
 
+  alias Servy.Wildthings
+
   def index(conv) do
-    json = Servy.Wildthings.list_bears() |> Poison.encode!
+    json = Wildthings.list_bears() |> Poison.encode!
     %{conv | status: 200, resp_body: json, resp_content_type: "application/json"}
   end
 
