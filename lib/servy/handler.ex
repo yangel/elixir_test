94dbcf46af6be087b86b15ad0cea40fc80c7b223
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-14 16:04:02 -0400

    using tasks

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -11,7 +11,7 @@ defmodule Servy.Handler do
   alias Servy.Conv, as: Conv
   alias Servy.BearController, as: BearController
   alias Servy.VideoCam
-  alias Servy.Fetcher
+  alias Servy.Tracker
 
   @doc "Transforms the request into a response."
   def handle(request) do
@@ -28,20 +28,17 @@ defmodule Servy.Handler do
     raise "Crash!"
   end
 
-  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
-    parent = self()
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
+    task = Task.async(fn -> Tracker.get_location "bigfoot" end)
 
-    Fetcher.async(fn -> VideoCam.snapshot("16x3i5") end)
-    Fetcher.async(fn -> VideoCam.snapshot("16x3i5") end)
-    Fetcher.async(fn -> VideoCam.snapshot("16x3i5") end)
+    snapshots =
+      ["16x3i5", "16x3i5", "16x3i5"]
+      |> Enum.map(&Task.async(fn -> VideoCam.snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
 
-    snapshot1 = Fetcher.get_result()
-    snapshot2 = Fetcher.get_result()
-    snapshot3 = Fetcher.get_result()
+    location = Task.await(task)
 
-    snapshots = [snapshot1, snapshot2, snapshot3]
-
-    %{conv | status: 200, resp_body: inspect(snapshots)}
+    %{conv | status: 200, resp_body: inspect({snapshots, location})}
   end
 
   def route(%Conv{method: "GET", path: "/hibernate/" <> time} = conv) do
---
+++ lib/servy/tracker.ex
@@ -0,0 +1,21 @@
+defmodule Servy.Tracker do
+  @moduledoc false
+
+  @doc """
+  Simulates sending a request to an external API
+  to get the GPS coordinates of a wildthing.
+  """
+  def get_location(wildthing) do
+    :timer.sleep(500)
+
+    locations = %{
+      "roscoe" => %{lat: "44.42354 N", lng: "110.5885 W"},
+      "smokey" => %{lat: "45.42354 N", lng: "110.5885 W"},
+      "brutus" => %{lat: "46.42354 N", lng: "110.5885 W"},
+      "bigfoot" => %{lat: "47.42354 N", lng: "110.5885 W"}
+    }
+
+    Map.get(locations, wildthing)
+  end
+  
+end
