4c238cb2366c4c54b74509ff792dbb21b9932944
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-14 16:38:15 -0400

    cleanUp: useing tasks in test

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -3,37 +3,23 @@ defmodule HttpServerTest do
 
   alias Servy.HttpServer
 
-  require Logger
-
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4001])
 
-    parent = self()
+    url = "http://localhost:4001/wildthings"
+    1..5
+      |> Enum.map(fn (_) -> Task.async(fn -> HTTPoison.get url end) end)
+      |> Enum.map(&Task.await/1)
+      |> Enum.map(&assert_success_response/1)
 
-    max_concurrent_requests = 5
+    {:ok, response} = Task.async(HTTPoison, :get, ["http://localhost:4001/bears/some_string"])
+      |> Task.await()
+    assert response.status_code == 404
 
-    for _ <- 1..max_concurrent_requests do
-      spawn(fn ->
-        {:ok, response} = HTTPoison.get "http://localhost:4001/wildthings"
-        send(parent, {:ok, response})
-      end)
-    end
+  end
 
-    for _ <- 1..max_concurrent_requests do
-      receive do
-        {:ok, response} ->
-          assert response.status_code == 200
-          assert response.body == "Bears, Lions, Tigers"
-      end
-    end
-
-    spawn(fn ->
-      {:ok, response} = HTTPoison.get "http://localhost:4001/bears/some_string"
-      send(parent, {:ok, response})
-    end)
-
-    receive do
-      {:ok, response} -> assert response.status_code == 404
-    end
+  defp assert_success_response({:ok, response}) do
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 end
