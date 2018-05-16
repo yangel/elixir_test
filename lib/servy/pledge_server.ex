0872112c652192c6035d633744b94a07290339fe
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-05-16 12:58:53 -0400

    added tags

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -32,28 +32,32 @@ defmodule Servy.PledgeServer do
   # Helper functions
 
   def call(pid, message) do
-    send pid, {self(), message}
+    send pid, {:call, self(), message}
     receive do {:response, response} -> response end
   end
 
   def cast(pid, message) do
-    send pid, message
+    send pid, {:cast, message}
   end
 
   # Server
   def listen_loop(state) do
     receive do
-      {sender, message} when is_pid(sender) ->
+      {:call, sender, message} when is_pid(sender) ->
         {response, new_state} = handle_call(message, state)
         send sender, {:response, response}
-        listen_loop new_state
-      :clear ->
-        new_state = []
+        listen_loop(new_state)
+      {:cast, message} ->
+        new_state = handle_cast(message, state)
         listen_loop(new_state)
       unexpected ->
         Logger.warn("Received unexpected message: #{inspect unexpected, pretty: true}")
         listen_loop(state)
     end
+  end
+
+  def handle_cast(:clear, _state) do
+    []
   end
 
   def handle_call(:total_pledged, state) do
