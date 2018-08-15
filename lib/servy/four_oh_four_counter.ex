2f566cce3be7cfe841f08292844e1589203ea48a
Evgeny Golomidov <egolomidov@tonicdesign.com>
2018-08-15 13:03:22 -0400

    refactored 404 server to use GenServer

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -3,15 +3,15 @@ defmodule Servy.FourOhFourCounter do
 
   @service_name __MODULE__
 
-  alias Servy.GenericServer
+  use GenServer
 
   # Client
   def start do
-    GenericServer.start(__MODULE__, %{}, @service_name)
+    GenServer.start(__MODULE__, %{}, name: @service_name)
   end
 
   def stop do
-    GenericServer.stop(@service_name)
+    GenServer.stop(@service_name)
   end
 
   def process_name do
@@ -19,37 +19,36 @@ defmodule Servy.FourOhFourCounter do
   end
 
   def bump_count(path) do
-    GenericServer.call @service_name, {:bump_count, path}
+    GenServer.call @service_name, {:bump_count, path}
   end
 
   def get_count(path) do
-    GenericServer.call @service_name, {:get_count, path}
+    GenServer.call @service_name, {:get_count, path}
   end
 
   def get_counts do
-    GenericServer.call @service_name, :get_counts
+    GenServer.call @service_name, :get_counts
   end
 
   def clear do
-    GenericServer.cast @service_name, :clear
+    GenServer.cast @service_name, :clear
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
-  def handle_call({:bump_count, path}, state) do
-    new_state = Map.update(state, path, 1, fn count -> count + 1 end)
-    {:ok, new_state}
+  def handle_call({:bump_count, path}, _from, state) do
+    new_state = Map.update(state, path, 1, &(&1 + 1))
+    {:reply, :ok, new_state}
   end
 
-  def handle_call({:get_count, path}, state) do
-    count = Map.get(state, path)
-    {count, state}
+  def handle_call({:get_count, path}, _from, state) do
+    {:reply, Map.get(state, path, 0), state}
   end
 
   def handle_cast(:clear, _state) do
-    %{}
+    {:no_reply, %{}}
   end
 
 end
